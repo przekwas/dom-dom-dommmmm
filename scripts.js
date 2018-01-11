106a9c844479df80dfeaa6f46b5357dd6d2329b4
Luke Przekwas <galactica@Lukes-MacBook-Pro.local>
2018-01-11 15:13:50 -0600

    if/else statements to determine actions

--- scripts.js
+++ scripts.js
@@ -21,18 +21,18 @@ document.addEventListener("DOMContentLoaded", function () {
         let idTag = document.createElement("h1");
         idTag.style.textShadow = "2px 0 0 white, -2px 0 0 white, 0 2px 0 white, 0 -2px 0 white, 1px 1px white, -1px -1px 0 white, 1px -1px 0 white, -1px 1px 0 white";
         square.appendChild(idTag);
-        let idText = document.createTextNode(square.id);
+        let idNumber = document.createTextNode(square.id);
 
         //slaps the box to the page
         body.appendChild(square);
 
         //makes the square have a mouseover effect to add its id
         square.addEventListener("mouseover", function () {
-            idTag.appendChild(idText);
+            idTag.appendChild(idNumber);
         })
         //makes the square have a mouseout effect to remove its id
         square.addEventListener("mouseout", function () {
-            idTag.removeChild(idText);
+            idTag.removeChild(idNumber);
         })
         //makes the square change to a random background color when clicked
         square.addEventListener("click", function () {
@@ -45,6 +45,24 @@ document.addEventListener("DOMContentLoaded", function () {
             //applies the bg color as a random rgb color to the square
             square.style.backgroundColor = randomRGB;
         })
+
+        //makes the square have a dbl click to remove itself
+        square.addEventListener("dblclick", function () {
+            //checks to see if square id number is even AND if it has any element on the right
+            if (divCounter % 2 === 0 && square.nextSibling == null) {
+                alert("There ain't no square after that to remove, yo!");
+            //checks to see if the square id number is just even AND if its not zero, then remove the next square as an action
+            } else if (divCounter % 2 === 0 && divCounter !== 0) {
+                body.removeChild(square.nextElementSibling);
+            }
+            //check to make sure user doesnt delete the click button
+            if (square.previousSibling.nodeName === "BUTTON") {
+                alert("There ain't no square before that to remove, yo!");
+            //checks to see if square id number is odd and remove the number before it as an action
+            } else if (divCounter % 2 !== 0) {
+                body.removeChild(square.previousSibling);
+            } 
+        })
     }
 
     //click button to make a box
@@ -52,3 +70,5 @@ document.addEventListener("DOMContentLoaded", function () {
         squareAdder();
     })
 })
+
+//body.removeChild(square);
